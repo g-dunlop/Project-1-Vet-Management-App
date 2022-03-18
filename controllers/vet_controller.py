91d5e862122d0b75e1a4856afd3cd5ec3b140d8f
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-03-18 19:37:17 +0000

    add page working adding vet to db

--- controllers/vet_controller.py
+++ controllers/vet_controller.py
@@ -9,3 +9,15 @@ vets_blueprint = Blueprint("vets", __name__)
 def vets():
     vets = vet_repository.select_all()
     return render_template("vets/index.html", vets = vets)
+
+@vets_blueprint.route("/vets/new", methods=['GET'])
+def new_vet():
+    return render_template("vets/new.html")
+
+@vets_blueprint.route("/vets", methods=['POST'])
+def create_vet():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    vet = Vet(first_name, last_name)
+    vet_repository.save(vet)
+    return redirect('/vets')
